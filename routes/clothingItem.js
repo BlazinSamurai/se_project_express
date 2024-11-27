1317594e4a405d1f82a8c7ae806d700685b5ca93
DamianLavin <damianmlavin@gmail.com>
2024-11-26 20:36:12 -0800

    clothingItem: routes and controllers refactored.

--- routes/clothingItem.js
+++ routes/clothingItem.js
@@ -1,8 +1,19 @@
 const router = require("express").Router();
-const { createItem } = require("../controllers/clothingItem");
+const {
+  createItem,
+  getItems,
+  updateItem,
+  deleteItem,
+} = require("../controllers/clothingItem");
 
 //C.R.U.D
 //Create
 router.post("/", createItem);
+//Read
+router.get("/", getItems);
+//Update
+router.put("/:itemId", updateItem);
+//Delete
+router.delete("/:itemId", deleteItem);
 
 module.exports = router;
