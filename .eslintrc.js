cc1b0d6ac79865b72ad5481087b5ea3bd0f3189a
DamianLavin <damianmlavin@gmail.com>
2024-12-17 20:38:15 -0800

    Updated 'arrow-body-style' rule to never.6.

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,7 @@ module.exports = {
     "no-underscore-dangle": ["error", { allow: ["_id"] }],
     "no-unused-vars": ["error", { vars: "local" }],
     "spaced-comment": ["error", "always", { markers: ["//"] }],
-    // "arrow-body-style": ["error", "always"],
+    "arrow-body-style": ["error", "never"],
     // "arrow-body-style": ["error", "as-needed"],
     // "arrow-body-style": ["error", "as-needed", { requireReturnForObjectLiteral: true },],
     // "arrow-body-style": ["error","as-needed",{ requireReturnForObjectLiteral: false },],
