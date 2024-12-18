4967e18d1433a85f2b4903fcf4b090ff4a4d76d5
DamianLavin <damianmlavin@gmail.com>
2024-12-17 21:06:42 -0800

    SE_PROJECT_EXPRESS.v1

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,11 +24,7 @@ module.exports = {
     "no-underscore-dangle": ["error", { allow: ["_id"] }],
     "no-unused-vars": ["error", { vars: "local" }],
     "spaced-comment": ["error", "always", { markers: ["//"] }],
-    // "arrow-body-style": ["error", "never"], DEFINITELY NOT IT!!!
     "arrow-body-style": ["error", "always"],
-    // "arrow-body-style": ["error", "as-needed"],
-    // "arrow-body-style": ["error", "as-needed", { requireReturnForObjectLiteral: true },],
-    // "arrow-body-style": ["error","as-needed",{ requireReturnForObjectLiteral: false },],
     "no-else-return": ["error", { allowElseIf: true }],
   },
 };
